3a4282323c187fe180614030d189bc34d9f79de1
cXiaof <310257509@qq.com>
2019-05-14 23:08:19 +0800

    publish first version

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 // new Map
 const map = new maptalks.Map('map', {
     center: [121.387, 31.129],
-    zoom: 14,
+    zoom: 13,
     baseLayer: new maptalks.TileLayer('base', {
         urlTemplate:
             'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
@@ -39,13 +39,60 @@ new maptalks.CompassControl({
     position: 'top-right'
 }).addTo(map)
 
-const gs = new maptalks.GeometrySelection()
-const layer = new maptalks.VectorLayer('sketchPad').addTo(map)
+const layerMarkers = new maptalks.VectorLayer('markers').addTo(map)
+const layerLines = new maptalks.VectorLayer('lines').addTo(map)
+const layerPolygons = new maptalks.VectorLayer('polygons').addTo(map)
+const gs = new maptalks.GeometrySelection({
+    layers: ['markers', 'lines', 'polygons']
+}).addTo(map)
+
+// add some geos
+const center = map.getCenter()
+const symbol = {
+    polygonFill: '#ebebeb',
+    polygonOpacity: 0.15,
+    lineColor: '#333'
+}
+new maptalks.Marker(center.add(0.01, 0.01)).addTo(layerMarkers)
+new maptalks.Marker(center.add(0.01, -0.01)).addTo(layerMarkers)
+new maptalks.Marker(center.add(-0.02, 0.02)).addTo(layerMarkers)
+new maptalks.Marker(center.add(-0.02, -0.02)).addTo(layerMarkers)
+new maptalks.LineString([
+    center.add(-0.015, -0.015),
+    center.add(-0.005, -0.005)
+]).addTo(layerLines)
+new maptalks.LineString([
+    center.add(0.005, 0.005),
+    center.add(0.015, 0.015)
+]).addTo(layerLines)
+new maptalks.Rectangle(center.add(-0.03, -0.02), 800, 1600, { symbol }).addTo(
+    layerPolygons
+)
+new maptalks.Rectangle(center.add(0, -0.02), 800, 1600, { symbol }).addTo(
+    layerPolygons
+)
+new maptalks.Rectangle(center.add(0.03, -0.02), 800, 1600, { symbol }).addTo(
+    layerPolygons
+)
 
 // new Toolbar
 const toolbar = new maptalks.control.Toolbar({
     position: 'top-left',
-    items: []
+    items: [
+        { item: 'enable', click: () => gs.enable() },
+        { item: 'disable', click: () => gs.disable() },
+        {
+            item: 'getGeometries',
+            click: () => {
+                const geos = gs.getGeometries()
+                const text = geos.reduce((target, geo) => {
+                    target += JSON.stringify(geo.toGeoJSONGeometry())
+                    return target
+                }, '')
+                alert(text || '[]')
+            }
+        }
+    ]
 }).addTo(map)
 
 // new tip Panel
@@ -53,7 +100,19 @@ const textPanel = new maptalks.control.Panel({
     position: 'bottom-left',
     draggable: true,
     custom: false,
-    content: ``,
+    content: `
+        Click <b>enable</b> to start to choose geometry,<br />
+        click it when it's color be changed.<br />
+        Click <b>disable</b> to pause. And you can click<br />
+        <b>enable</b> again, geometries chosen will be<br />
+        retained.<br />
+        Click <b>getGeometries</b> and check it.<br />
+        <br />
+        点击<b>enable</b>开始选择图形，被命中时图形会变色。<br />
+        点击<b>disable</b>暂停选择。你可以再次点击<b>enable</b><br />
+        继续选择，上一次选择的图形会被保留。<br />
+        点击<b>getGeometries</b>检查是否为选择的图形。<br />
+    `,
     closeButton: true
 })
 map.addControl(textPanel)
--- index.js
+++ index.js
@@ -1,37 +1,73 @@
+import isEqual from 'lodash.isequal'
 import { createFilter, getFilterFeature } from '@maptalks/feature-filter'
 
-const layerPrevent = maptalks.INTERNAL_LAYER_PREFIX
+const uid = 'geometryselection@cXiaof'
 const options = {
     layers: [],
+    availTypes: '*',
     colorHit: '#ffa400',
-    colorChoose: '#00bcd4'
+    colorChosen: '#00bcd4'
 }
 
-export class GeometrySelection extends maptalks.Class {
+export class GeometrySelection extends maptalks.Eventable(maptalks.Class) {
     constructor(options, defaultChosenGeos) {
         super(options)
-        this._layer = `${layerPrevent}geometryselection@cXiaof`
+        this._layerId = `${maptalks.INTERNAL_LAYER_PREFIX}${uid}`
         this._enabled = false
         this._setDefaultChosenGeos(defaultChosenGeos)
+        this._markerStyleDefault = {
+            markerType: 'path',
+            markerPath: [
+                {
+                    path:
+                        'M8 23l0 0 0 0 0 0 0 0 0 0c-4,-5 -8,-10 -8,-14 0,-5 4,-9 8,-9l0 0 0 0c4,0 8,4 8,9 0,4 -4,9 -8,14z M3,9 a5,5 0,1,0,0,-0.9Z',
+                    fill: '#DE3333'
+                }
+            ],
+            markerPathWidth: 16,
+            markerPathHeight: 23,
+            markerWidth: 24,
+            markerHeight: 34
+        }
+    }
+
+    addTo(map) {
+        if (!map) return this
+        if (map._map_tool) map._map_tool.disable()
+        this._map = map
+        this._layer = new maptalks.VectorLayer(this._layerId)
+        this._layer.addTo(map).bringToFront()
+        return this
+    }
+
+    getMap() {
+        return this._map
     }
 
     enable() {
         this._enabled = true
+        this._registerEvents()
+        if (this._layer) this._layer.show()
+        this.fire('enable')
         return this
     }
 
     disable() {
+        if (!this._enabled) return this
         this._enabled = false
+        this._offMapEvents()
+        if (this._layer) this._layer.hide()
+        this.fire('disable')
         return this
     }
 
     toggleEnable() {
-        this._enabled = !this._enabled
+        this._enabled ? this.disable() : this.enable()
         return this
     }
 
     isEnabled() {
-        return this._enabled
+        return !!this._enabled
     }
 
     getGeometries() {
@@ -39,20 +75,18 @@ export class GeometrySelection extends maptalks.Class {
     }
 
     forEach(fn = () => false, context) {
-        if (typeof fn !== 'function') return this
-        const geos = this.getGeometries()
-        geos.forEach((geo, i, arr) => {
+        if (!this._isFn(fn)) return this
+        this._geometries.forEach((geo, i, arr) => {
             if (context) fn.call(context, geo, i, arr)
             else fn(geo, i, arr)
         })
         return this
     }
 
-    filter(fn, context) {
-        const isFn = typeof fn === 'function'
+    filter(fn = () => false, context) {
+        const isFn = this._isFn(fn)
         const filter = isFn ? fn : createFilter(fn)
-        const geos = this.getGeometries()
-        return geos.filter((geo, i, arr) => {
+        return this._geometries.filter((geo, i, arr) => {
             geo = isFn ? geo : getFilterFeature(geo)
             return context
                 ? filter.call(context, geo, i, arr)
@@ -60,12 +94,158 @@ export class GeometrySelection extends maptalks.Class {
         })
     }
 
+    clear() {
+        if (this._layer) this._layer.clear()
+        this._geometries = []
+    }
+
     remove() {
+        this.disable()
+        if (this._layer) this._layer.remove()
+        delete this.hitGeo
+        delete this._layer
+        delete this._layerId
+        delete this._enabled
+        delete this._markerStyleDefault
+        delete this._map
+        delete this._geometries
+        delete this._mousemove
+        delete this._click
         return this
     }
 
     _setDefaultChosenGeos(geos) {
-        this._geometries = geos instanceof Array ? geos : []
+        this._geometries = this._isArrayHasData(geos) ? geos : []
+    }
+
+    _isFn(fn) {
+        return maptalks.Util.isFunction(fn)
+    }
+
+    _isArrayHasData(attr) {
+        return maptalks.Util.isArrayHasData(attr)
+    }
+
+    _registerEvents() {
+        const layers = this.options['layers']
+        if (this._isArrayHasData(layers)) {
+            const map = this._map
+            this._mousemove = this._mousemoveEvents.bind(this)
+            this._click = this._clickEvents.bind(this)
+            map.on('mousemove', this._mousemove, this)
+            map.on('click', this._click, this)
+        }
+    }
+
+    _offMapEvents() {
+        if (this._mousemove) {
+            const map = this._map
+            map.off('mousemove', this._mousemove, this)
+            map.off('click', this._click, this)
+        }
+    }
+
+    _mousemoveEvents(e) {
+        const { target, coordinate } = e
+        const layers = this.options['layers']
+        const tolerance = 0.0001
+        target.identify({ coordinate, tolerance, layers }, (geos) => {
+            const hit = geos.reduce((target, geo) => {
+                if (target) return target
+                if (this._isAvailTypes(geo)) return geo
+                return target
+            }, null)
+            this.hitGeo = hit
+            this._showCopyHitGeo()
+        })
+    }
+
+    _isAvailTypes(geo) {
+        const availTypes = this.options['availTypes']
+        if (availTypes === '*') return true
+        if (!this._isArrayHasData(availTypes)) return false
+        const type = geo.getType()
+        return availTypes.reduce(
+            (avail, availType) => avail || type.endsWith(availType),
+            false
+        )
+    }
+
+    _showCopyHitGeo(geo) {
+        const id = `__hit__${uid}`
+        const lastHitGeo = this._layer.getGeometryById(id)
+        if (lastHitGeo) lastHitGeo.remove()
+        if (this.hitGeo) {
+            this.fire('hit', this.hitGeo)
+            const hitSymbol = this._getSymbolOrDefault('Hit')
+            const hitCopy = this._copyGeoUpdateSymbol(hitSymbol)
+            hitCopy.setId(id)
+        }
+    }
+
+    _getSymbolOrDefault(colorType, geo = this.hitGeo) {
+        let symbol = geo.getSymbol()
+        const type = geo.getType()
+        const color = this.options[`color${colorType}`]
+        const lineWidth = 4
+        if (symbol) {
+            for (let key in symbol) {
+                if (key.endsWith('Fill') || key.endsWith('Color'))
+                    symbol[key] = color
+            }
+            symbol.lineWidth = lineWidth
+        } else {
+            if (type.endsWith('Point'))
+                symbol = Object.assign(this._markerStyleDefault, {
+                    markerFill: color
+                })
+            else symbol = { lineColor: color, lineWidth }
+        }
+        return symbol
+    }
+
+    _copyGeoUpdateSymbol(symbol, geo = this.hitGeo) {
+        return geo
+            .copy()
+            .updateSymbol(symbol)
+            .addTo(this._layer)
+    }
+
+    _clickEvents() {
+        if (this.hitGeo) {
+            const coordHit = this._getSafeCoords(this.hitGeo)
+            const chooseNext = this._geometries.reduce((chooses, geo) => {
+                const coord = this._getSafeCoords(geo)
+                if (isEqual(coordHit, coord)) return chooses
+                return [...chooses, geo]
+            }, [])
+            if (chooseNext.length === this._geometries.length) {
+                this.fire('choose', this.hitGeo)
+                const newGeo = this.hitGeo.copy()
+                this._geometries.push(this.hitGeo)
+            } else this._geometries = chooseNext
+            this._renderChosenGeos()
+        }
+    }
+
+    _getSafeCoords(geo) {
+        const { options } = geo
+        geo = geo.copy()
+        let coords = geo.getCoordinates()
+        if (options.numberOfShellPoints) {
+            options.numberOfShellPoints *= 4
+            geo.setOptions(options)
+            coords = [geo.getShell()]
+        }
+        return coords
+    }
+
+    _renderChosenGeos() {
+        this._layer.clear()
+        this._geometries.forEach((geo) => {
+            const chooseSymbol = this._getSymbolOrDefault('Chosen', geo)
+            this._copyGeoUpdateSymbol(chooseSymbol, geo)
+        })
     }
 }
 
